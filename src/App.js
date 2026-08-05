dabccbd21931e5d83d1641f07b073fc150c93259
eeeeee <cindy.pei@scs.on.ca>
2026-08-04 22:28:45 -0400

    fdskjaf

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <App />
     </HashRouter>
     </React.StrictMode>
